a39d1d6b8a2bb355bfffe22b665cad2cbe32f528
ohumen <74374412+ohumen@users.noreply.github.com>
2021-11-24 11:59:16 +0200

    final

--- lab4/lab4.hs
+++ lab4/lab4.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# OPTIONS_GHC -Wno-deferred-out-of-scope-variables #-}
 -- Лабораторна робота №4
 -- студентки групи КН-32 підгрупи 1
 -- Гумен Ольга
@@ -14,7 +16,41 @@
 
 -- 10. статистика публiкацiй автора — кiлькiсть, обсяг (у сторiнках), тип;
 
+type Author  = String
+type Name = String 
+type SourceName = String
+type Year = Int
+type SourceNumber = Int
+type Pages = Int
+type City = String
 
+data Publitactions = Book Author Name City SourceName Year |
+                     Article Author Name SourceName Year SourceNumber Pages |
+                     ThesisOfReport Author Name SourceName City Year Pages
+                     deriving (Eq, Show)
+
+publitactions :: [Publitactions]
+publitactions = [
+    Book "Ernest Hemingway" "Islands in the Stream: A Novel" "Havana" "Scribner" 1970,
+    Book "Paulo Coelho" "By the River Piedra I Sat Down and Wept: A Novel of Forgiveness" "Paris" "HarperCollins" 2009,
+    Book "Ernest Hemingway" "The Complete Short Stories Of Ernest Hemingway: The Finca Vigia Edition" "Berlin" "Scribner" 1998,
+    
+    Article "Warren Winick-Ng" "Cell-type specialization is encoded by specific chromatin topologies" "Nature" 2021 19 200,
+    Article "Can Cao" "Structure, function and pharmacology of human itch GPCRs" "Nature" 2021 20 150,
+    
+    ThesisOfReport "Andrés Guadamuz" "Analysis of UK/EU Law on Data Mining in Higher Education Institutions" "University of Sussex" "London" 2013 30]
+
+
+applyAll :: [a -> b] -> [a] -> [b]
+applyAll  _ [] = []
+applyAll (x:xs) (y:ys) = x y : applyAll xs ys
+
+function :: String -> [Publitactions] -> String
+function _ [] = "False"
+function x (y:ys) = if x == Author y then Name Pages publitactions y else function x ys
+
+check :: String -> [String]
+check a = [function a publitactions]
 
 -- Висновок: Під час даної лабораторної роботи я ознайомилась з системою типiв та класiв типiв. 
 -- Також я набула досвiду визначення нових типiв та класiв типiв i їх використання після чого застосувала вивчене на практиці.
